092cfe27cfc4222329616690c31b6a46dfa51ffa
Santhusha <santhushajanana@gmail.com>
2023-03-14 00:20:54 +0530

    Version 1.0.1.

--- script.js
+++ script.js
@@ -3,41 +3,43 @@ const searchInput = document.getElementById('search');
 const results = document.getElementById('results');
 const audioPlayer = document.getElementById('audio-player');
 
-searchButton.addEventListener('click', () => {
-    const query = searchInput.value;
+searchButton.addEventListener("click", () => {
+  const query = searchInput.value;
 
-    if (query) {
-        search(query);
-    }
+  if (query) {
+    search(query);
+  }
 });
-
+cd 
 function search(query) {
-    fetch(`https://api.deezer.com/search?q=${query}`)
-        .then(response => response.json())
-        .then(data => {
-            results.innerHTML = '';
-            data.data.forEach(song => {
-                const songDiv = document.createElement('div');
-                songDiv.className = 'song';
+  fetch(
+    `https://cors-anywhere.herokuapp.com/https://api.deezer.com/search?q=${query}`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      results.innerHTML = "";
+      data.data.forEach((song) => {
+        const songDiv = document.createElement("div");
+        songDiv.className = "song";
 
-                const image = document.createElement('img');
-                image.src = song.album.cover_big;
-                songDiv.appendChild(image);
+        const image = document.createElement("img");
+        image.src = song.album.cover_big;
+        songDiv.appendChild(image);
 
-                const title = document.createElement('h2');
-                title.innerText = song.title;
-                songDiv.appendChild(title);
+        const title = document.createElement("h2");
+        title.innerText = song.title;
+        songDiv.appendChild(title);
 
-                const artist = document.createElement('h3');
-                artist.innerText = song.artist.name;
-                songDiv.appendChild(artist);
+        const artist = document.createElement("h3");
+        artist.innerText = song.artist.name;
+        songDiv.appendChild(artist);
 
-                songDiv.addEventListener('click', () => {
-                    audioPlayer.src = song.preview;
-                    audioPlayer.play();
-                });
-
-                results.appendChild(songDiv);
-            });
+        songDiv.addEventListener("click", () => {
+          audioPlayer.src = song.preview;
+          audioPlayer.play();
         });
+
+        results.appendChild(songDiv);
+      });
+    });
 }
